d8404a7f536b7c9b2563887d7acb8d065d538c3f
waziri123 <waziriallyamiri123@gmail.com>
2023-05-31 05:19:06 -0700

    first commit

---
+++ test.php
@@ -0,0 +1,37 @@
+
+CREATE SALE INVOICE
+ROUTE
+{company_id}\sales\invoices\create\
+VIEWS
+\wamp64\www\akaunting-1\resources\views\sales\invoices\create.blade.php
+DATA: NO
+SUBCOMPONENT
+\wamp64\www\akaunting-1\app\View\Components\Layouts\Admin.php
+\wamp64\www\akaunting-1\resources\views\components\documents\form\content.blade.php
+\wamp64\www\akaunting-1\resources\views\components\documents\script.blade.php
+Slots         
+title,favorite,content
+SUBCOMPONENTLEVEL1(1)
+\wamp64\www\akaunting-1\resources\views\components\layouts\admin\head.blade.php
+\wamp64\www\akaunting-1\resources\views\components\layouts\admin\menu.blade.php
+\wamp64\www\akaunting-1\resources\views\components\layouts\admin\header.blade.php
+\wamp64\www\akaunting-1\resources\views\components\menu\favorite.blade.php
+\wamp64\www\akaunting-1\resources\views\components\layouts\admin\content.blade.php
+\wamp64\www\akaunting-1\resources\views\livewire\notification\browser\firefox.blade.php
+\wamp64\www\akaunting-1\resources\views\components\layouts\admin\footer.blade.php
+\wamp64\www\akaunting-1\resources\views\components\layouts\admin\scripts.blade.php
+
+DATA:
+$metaTitle-slot from parent
+$title-slot from parent
+$content-slot from parent
+$status-slot from parent
+$info-slot from parent
+$favorite-slot from parent
+
+Slots         
+metaTitle,title,status,info,favorite,buttons,moreButtons
+
+SUBCOMPONENTLEVEL2(1)
+\wamp64\www\akaunting-1\resources\views\components\layouts\pwa\head.blade.php
+\wamp64\www\akaunting-1\resources\views\components\script\exceptions\trackers.blade.php
